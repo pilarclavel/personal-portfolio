9dd9244d78c00fa759e5c19ca75f6072c7af97fa
Pilar Clavel <95171501+pilarclavel@users.noreply.github.com>
2022-10-18 12:07:39 +0200

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About me</title>
+    <link rel="stylesheet" href="./css/about.css">
+    <script src="./js/about.js" defer></script>
+</head>
+
+<body>
+    <section class="aboutme" id="aboutme">
+
+    <a href="./home.php">
+  <label for="menu"class="back"> <img src="./images/proximo.png" alt=""> </label>
+ </a>
+        <div class="abouttitle">
+            <h4>01</h4>
+            <h3>Acerca de </h3>
+            <h2 class="mi">mí</h2>
+        </div>
+        <div class="aboutmediv">
+            <p>Empecé hace cuatro años a diseñar y a crear contenido. Al principio eran logotipos, alguna identidad
+                corporativa... y eso me hizo descubrir cuánto me gusta esto, cuánto me gusta el diseño y la creación de
+                contenido. Al fin había encontrado mi punto fuerte y a su vez, tenía tanta suerte, que me encantaba lo
+                que hacía. Fue cuando comencé a diseñar y realizar páginas webs, a crear contenido para redes sociales y
+                a diseñar propuestas de campañas publicitarias. Estoy segura de que me queda mucho por aprender, pero
+                hasta el momento, estoy disfrutando del camino y del aprendizaje cada día.</p>
+
+            <div class="aboutimage" data-aos="fade-right">
+                <img src="images/silueta-buena.jpg" alt="">
+            </div>
+        </div>
+        <section class="aboutcuadro">
+            <h5>“La clave principal para crecer y evolucionar
+                consiste en estar dispuestos a descubrir aquello que desconocemos”</h5>
+        </section>
+        <section class="aboutgrid">
+            <h3>CÓMO SOY</h3>
+            <div class="aboutrow">
+                <div class="grid">
+                    <h4>CURIOSA</h4>
+                    <p>Es una de mis grandes virtudes y la que me empuja día a día a superarme un poco más. Gracias a
+                        esto, no paro de aprender sobre todo lo que me rodea.</p>
+                </div>
+                <div class="grid2">
+                    <h4>CREATIVA</h4>
+                    <p>Si hay algo que me gusta, son las cosas diferentes, las que no tienen que seguir un patrón. Es lo
+                        que hace que en cada diseño se vea reflejada una parte de mí.</p>
+                </div>
+                <div class="grid3">
+                    <h4>PROACTIVA</h4>
+                    <p>Algo que me caracteriza, es la energía que irradio a mi alrededor, las ganas que tengo siempre de
+                        aprender y de conseguir mi mejor versión.</p>
+                </div>
+                <div class="grid4">
+                    <h4>AMBICIOSA</h4>
+                    <p>Soy consciente del largo camino que me queda por recorrer, pero lo afronto con ganas de superarme
+                        y como una oportunidad de aprendizaje.</p>
+                </div>
+        </section>
+        <section class="aboutgrid">
+            <h3>INFORMÁTICA</h3>
+        </section> 
+        <div class="slideshow-container">
+
+            <div class="aboutcirculos mySlides fade">
+                <div class="circulos">
+                    <div class=contcirc>
+                        <span>4.9</span>
+                        <h4>Figma</h4>
+                    </div>
+                </div>
+                <div class="circulos">
+                    <div class=contcirc>
+                        <span>4.6</span>
+                        <h4>InDesign</h4>
+                    </div>
+                </div>
+                <div class="circulos">
+                    <div class=contcirc>
+                        <span>4.1</span>
+                        <h4>Illustrator</h4>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="aboutcirculos mySlides fade">
+                <div class="circulos2">
+                    <div class=contcirc>
+                        <span>3.9</span>
+                        <h4>Photoshop</h4>
+                    </div>
+                </div>
+                <div class="circulos2">
+                    <div class=contcirc>
+                        <span>3.5</span>
+                        <h4>Miro</h4>
+                    </div>
+                </div>
+                <div class="circulos2">
+                    <div class=contcirc>
+                        <span>3.2</span>
+                        <h4>Wordpress</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+       <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+        </div>
+
+        <!-- Full-width images with number and caption text -->
+
+
+        <section class="exterior">
+            <div class="estudios">
+                <h3>ESTUDIOS</h3>
+                <div class="aboutfirst">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,17.18V13.18L12,17L5,13.18Z" />
+                    </svg>
+                    <p>Grado en Publicidad y Relaciones Públicas</p>
+                </div>
+                <div class="aboutfirst">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M4,6H20V16H4M20,18A2,2 0 0,0 22,16V6C22,4.89 21.1,4 20,4H4C2.89,4 2,4.89 2,6V16A2,2 0 0,0 4,18H0V20H24V18H20Z" />
+                    </svg>
+                    <p>Máster en Marketing Digital, UX/UI y Desarrollo Web</p>
+                </div>
+            </div>
+
+            <div class="idiomas">
+                <h3>IDIOMAS</h3>
+                <div class="abouttwo">
+                    <p>Español</p>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                </div>
+                <div class="abouttwo">
+                    <p class="ingl">Inglés</p>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+                    </svg>
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M12 2L9.19 8.62L2 9.24L7.45 13.97L5.82 21L12 17.27V2Z" />
+                    </svg>
+                </div>
+            </div>
+        </section>
+        <a href="./proyectos.php"> <button class="aboutdiscover">CONOCE MIS PROYECTOS</button></a>
+
+</body>
+
+</html>
